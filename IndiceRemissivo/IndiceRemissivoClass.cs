4ccf512c4e355509022e0fac153356be316fe193
silveiraluisf <silveiraluis88@gmail.com>
2022-11-28 11:36:42 -0300

    Indice remissivo conta o numero de ocorrencias das palavras e ordena em ordem alfabetica

--- IndiceRemissivo/IndiceRemissivoClass.cs
+++ IndiceRemissivo/IndiceRemissivoClass.cs
@@ -1,14 +1,45 @@
-﻿using System.Reflection.PortableExecutable;
-using System.Text;
+﻿using System.IO;
 
 namespace IndiceRemissivo
 {
     public class IndiceRemissivoClass
     {
-        public static void IndiceRemissivo(string pathTXT, string pathIgnore)
+        public static void IndiceRemissivo(string pathTXT)
         {
-            pathTXT = "C:\\Users\\silve\\source\\repos\\ExerciciosRevisaoParte3\\IndiceRemissivo\\texto.txt";
-            var file = File.ReadAllText(pathTXT).Split(new[] { ' ' });
+            //Publicação utilizada como base para desenvolver a atividade: https://blog.groupdocs.com/parser/count-words-and-occurrences-using-csharp/
+
+            Dictionary<string, int> stats = new Dictionary<string, int>();
+            var text= File.ReadAllText(pathTXT);
+            char[] chars = { ' ', '.', ',', ';', ':', '?', '\n', '\r' };
+
+            //separa palavras
+            string[] words = text.Split(chars);
+            int minWordLength = 2;
+
+            //interacao sobre as palavras para contar o numero de ocorrencias
+            foreach(string word in words)
+            {
+                string w = word.Trim().ToLower();
+                if(w.Length > minWordLength )
+                {
+                    if (!stats.ContainsKey(w))
+                    {
+                        stats.Add(w, 1); //add nova palavra a colecao
+                    }
+                    else
+                    {
+                        stats[w] += 1; //atualiza o contador de ocorrencias de palavras
+                    }
+                }
+            }
+
+            var ordenerStats = stats.OrderBy(x => x.Key);
+
+            foreach(var pair in ordenerStats)
+            {
+                Console.WriteLine("Total de ocorrências de {0}: {1}" , pair.Key, pair.Value);
+            }
+
         }
         public static void Imprime()
         {
--- IndiceRemissivo/Program.cs
+++ IndiceRemissivo/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using IndiceRemissivo;
+
+namespace IndiceRemissivo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string pathTXT;
+            //string pathIgnore;
+            Console.WriteLine("---- PROGRAMA INDICE REMISSIVO ----");
+            pathTXT = "C:\\Users\\silve\\source\\repos\\ExerciciosRevisaoParte3\\IndiceRemissivo\\texto.txt";
+            //pathIgnore = "C:\\Users\\silve\\source\\repos\\ExerciciosRevisaoParte3\\IndiceRemissivo\\ignore.txt";
+            IndiceRemissivoClass.IndiceRemissivo(pathTXT);
+
+        }
+    }
+}
+
+
+
+
