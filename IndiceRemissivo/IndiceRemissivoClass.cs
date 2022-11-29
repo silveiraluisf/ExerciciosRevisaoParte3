bb0d9a5a68ab8e16da5a1a5893b26f480e33a0b7
silveiraluisf <silveiraluis88@gmail.com>
2022-11-29 12:46:13 -0300

    Contador de ocorrências de palavras agora usa o métrodo RealAllLines

--- IndiceRemissivo/IndiceRemissivoClass.cs
+++ IndiceRemissivo/IndiceRemissivoClass.cs
@@ -5,61 +5,43 @@
         public static void IndiceRemissivo(string pathTXT)
         {
             //Fonte utilizada como base para desenvolver a atividade: https://blog.groupdocs.com/parser/count-words-and-occurrences-using-csharp/
-
-            Dictionary<string, int> stats = new Dictionary<string, int>();
-            var text= File.ReadAllText(pathTXT);
+            Dictionary<string, int> stats = new();
+            string[] lines = File.ReadAllLines(pathTXT);
             char[] chars = { ' ', '.', ',', ';', ':', '?', '<', '>', '\\', '/', '|', '~', '^', '´', '`', '[', ']', '{', '}', '‘', '“', '!', '@', '#', '$', '%', '&', '&', '(', ')', '_', '+', '=', '\n', '\r' };
-
-            //separa palavras
-            string[] words = text.Split(chars);
-            int minWordLength = 2;
-
-            //interacao sobre as palavras para contar o numero de ocorrencias
-            foreach(string word in words)
+            int counter = 0;
+            // Display the file contents by using a foreach loop.
+            foreach (string line in lines)
             {
-                string w = word.Trim().ToUpper();
-                if(w.Length > minWordLength )
+                string[] words = line.Split(chars);
+                counter++;
+                int minWordLength = 2;
+                //interacao sobre as palavras para contar o numero de ocorrencias
+                foreach (string word in words)
                 {
-                    if (!stats.ContainsKey(w))
+                    string w = word.Trim().ToUpper();
+                    if (w.Length > minWordLength)
                     {
-                        stats.Add(w, 1); //add nova palavra a colecao
-                    }
-                    else
-                    {
-                        stats[w] += 1; //atualiza o contador de ocorrencias de palavras
+                        if (!stats.ContainsKey(w))
+                        {
+                            stats.Add(w, 1); //add nova palavra a colecao                         
+                        }
+                        else
+                        {
+                            stats[w] += 1; //atualiza o contador de ocorrencias de palavras
+                        }
                     }
                 }
             }
-
             var ordenerStats = stats.OrderBy(x => x.Key);
-            Imprime(ordenerStats);
-          
+            Imprime(ordenerStats);          
         }
         public static void Imprime(IOrderedEnumerable<KeyValuePair<string, int>> a)
-        {
+        {            
             foreach (var pair in a)
             {
                 Console.WriteLine("{0} ({1})", pair.Key, pair.Value);
             }
         }
-
-        public static void LinesCount(string pathTXT)
-        {
-
-           // string line = File.ReadLines(pathTXT);
-            
-                int counter = 0;
-
-                // Read the file and display it line by line.  
-                foreach (string line in System.IO.File.ReadLines(@"c:\test.txt"))
-                {
-                    System.Console.WriteLine(line);
-                    counter++;
-                }
-
-                System.Console.WriteLine("There were {0} lines.", counter);
-                // Suspend the screen.  
-                System.Console.ReadLine();
-        }
     }
+    
 }
