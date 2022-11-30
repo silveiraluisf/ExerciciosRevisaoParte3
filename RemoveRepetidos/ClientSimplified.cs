251b765cb28cdb952d80f518063f350c83045809
silveiraluisf <silveiraluis88@gmail.com>
2022-11-30 17:32:08 -0300

    Criação da classe ClientSimplified para testes

---
+++ RemoveRepetidos/ClientSimplified.cs
@@ -0,0 +1,15 @@
+﻿namespace RemoveRepetidos
+{
+    internal class ClientSimplified
+    {
+        public string Name { get; set; } 
+        public long CPF { get; set; }
+
+        public ClientSimplified(string clientName, long clientCPF)
+        {
+            clientName = Name;
+            clientCPF = CPF;
+        }
+
+    }
+}
--- RemoveRepetidos/Program.cs
+++ RemoveRepetidos/Program.cs
@@ -1,4 +1,5 @@
 ﻿using RemoveRepetidos;
+using System.Security.Cryptography;
 
 public class Program
 {
@@ -6,11 +7,24 @@ public class Program
     //Método para remoção de obj repetidos usando ToHashSet: https://code-maze.com/remove-duplicates-from-a-list-csharp/
     public static void Main()
     {
+        //criação da lista com inteiros para teste
         var testListInt = new MyList<int>();
         testListInt.ListWithDuplicates = new List<int>() { 1, 2, 3, 4, 5, 1, 2, 3, 4 };
+
+        //criação da lista de strings para teste
         var testListString = new MyList<string>();
         testListString.ListWithDuplicates = new List<string>() { "a", "b", "c", "d", "a", "e", "f", "e"};
 
+        //criação da lista de clientes e objetos da classe ClientSimplified para teste
+        var testListClient = new MyList<ClientSimplified>();
+        ClientSimplified a = new("a", 123123123);
+        ClientSimplified b = new("b", 456456456);
+        ClientSimplified c = new("c", 789789789);
+        ClientSimplified d = new("a", 123123123);
+        ClientSimplified e = new("e", 123456789);
+        testListClient.ListWithDuplicates = new List<ClientSimplified>() { a, b, c, d, e };
+
+        //execução do método para remover os itens repetidos
         Console.WriteLine("---- REMOVEDOR DE OBJETOS REPETIDOS NA LISTA ----");
      
         Console.WriteLine("\nLISTA INICIAL COM INTEIROS REPETIDOS:");
@@ -25,5 +39,9 @@ public class Program
         Console.WriteLine("\nRESULTADO DA LISTA APÓS A REMOÇÃO DOS OBJETOS REPETIDOS:");
         Console.WriteLine("Lista = {0}", string.Join(",", testListString.ListWithDuplicates.ConvertingToHashSet()));
 
+        Console.WriteLine("\nLISTA INICIAL COM OBJETOS CLIENTES REPETIDOS:");
+        
+        Console.WriteLine("\nRESULTADO DA LISTA APÓS A REMOÇÃO DOS OBJETOS REPETIDOS:");
+      
     }
 }
