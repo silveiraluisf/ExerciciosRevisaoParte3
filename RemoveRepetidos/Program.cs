4ea61205068c0ae9f8d13876a07b4abc9e6d8ab8
silveiraluisf <silveiraluis88@gmail.com>
2022-11-30 16:36:26 -0300

    Removedor de obj repetidos testada para lista de string

--- RemoveRepetidos/Program.cs
+++ RemoveRepetidos/Program.cs
@@ -8,14 +8,22 @@ public class Program
     {
         var testListInt = new MyList<int>();
         testListInt.ListWithDuplicates = new List<int>() { 1, 2, 3, 4, 5, 1, 2, 3, 4 };
+        var testListString = new MyList<string>();
+        testListString.ListWithDuplicates = new List<string>() { "a", "b", "c", "d", "a", "e", "f", "e"};
 
         Console.WriteLine("---- REMOVEDOR DE OBJETOS REPETIDOS NA LISTA ----");
-        //Console.WriteLine("");
-        Console.WriteLine("\nLISTA INICIAL COM OBJETOS REPETIDOS:");
+     
+        Console.WriteLine("\nLISTA INICIAL COM INTEIROS REPETIDOS:");
         Console.WriteLine("Lista = {0}", string.Join(",", testListInt.ListWithDuplicates));
 
         Console.WriteLine("\nRESULTADO DA LISTA APÓS A REMOÇÃO DOS OBJETOS REPETIDOS:");
         Console.WriteLine("Lista = {0}", string.Join(",", testListInt.ListWithDuplicates.ConvertingToHashSet()));
+
+        Console.WriteLine("\nLISTA INICIAL COM STRING REPETIDAS:");
+        Console.WriteLine("Lista = {0}", string.Join(",", testListString.ListWithDuplicates));
+
+        Console.WriteLine("\nRESULTADO DA LISTA APÓS A REMOÇÃO DOS OBJETOS REPETIDOS:");
+        Console.WriteLine("Lista = {0}", string.Join(",", testListString.ListWithDuplicates.ConvertingToHashSet()));
 
     }
 }
