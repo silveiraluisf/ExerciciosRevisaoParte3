a6ddb4931fc676b36f66b22e1da609060639413f
silveiraluisf <silveiraluis88@gmail.com>
2022-11-30 11:07:22 -0300

    Ajuste do nome da variável de teste para lista de inteiros

--- RemoveRepetidos/Program.cs
+++ RemoveRepetidos/Program.cs
@@ -1,5 +1,4 @@
 ﻿using RemoveRepetidos;
-using System.Linq;
 
 public class Program
 {
@@ -7,16 +6,16 @@ public class Program
     //Método para remoção de obj repetidos usando ToHashSet: https://code-maze.com/remove-duplicates-from-a-list-csharp/
     public static void Main()
     {
-        var testeList = new MyList<int>();
-        testeList.ListWithDuplicates = new List<int>() { 1, 2, 3, 4, 5, 1, 2, 3, 4 };
+        var testListInt = new MyList<int>();
+        testListInt.ListWithDuplicates = new List<int>() { 1, 2, 3, 4, 5, 1, 2, 3, 4 };
 
         Console.WriteLine("---- REMOVEDOR DE OBJETOS REPETIDOS NA LISTA ----");
         //Console.WriteLine("");
         Console.WriteLine("\nLISTA INICIAL COM OBJETOS REPETIDOS:");
-        Console.WriteLine("Lista = {0}", string.Join(",", testeList.ListWithDuplicates));
+        Console.WriteLine("Lista = {0}", string.Join(",", testListInt.ListWithDuplicates));
 
         Console.WriteLine("\nRESULTADO DA LISTA APÓS A REMOÇÃO DOS OBJETOS REPETIDOS:");
-        Console.WriteLine("Lista = {0}", string.Join(",", testeList.ListWithDuplicates.ConvertingToHashSet()));
+        Console.WriteLine("Lista = {0}", string.Join(",", testListInt.ListWithDuplicates.ConvertingToHashSet()));
 
     }
 }
